9c18093657a740a7d6f4a431747cd1d8ff989a23
Sam Arita <arita.sam@student.moringaschool.com>
2022-08-16 17:13:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(a){
+ let distanceFromHq = Math.abs(a-42);
+
+ return distanceFromHq;
+}
+distanceFromHqInBlocks(34)
+function distanceFromHqInFeet(b){
+    let feet = distanceFromHqInBlocks(b) 
+    return feet * 264;
+}
+distanceFromHqInFeet(43);
+function distanceTravelledInFeet(c, d){
+  return Math.abs((c-d)*264)
+}
+function calculatesFarePrice(start, destination){
+    let price;
+    let proximity= Math.abs((destination-start)*264)
+    if (proximity <= 400){
+        price = 0
+    }
+    else if (proximity>400 && proximity<=2000){
+        
+        price = 0.02 * (proximity-400);
+    }
+    else if (proximity > 2000 && proximity <= 2500){
+        price = 25 
+    }
+    else if (proximity > 2500){
+        return 'cannot travel that far'
+    }
+    return price;
+}
